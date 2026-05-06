0351427f6b46ceeff8a6142669d7d840bde310da
Xetirt <g1xaviert@gmail.com>
2026-05-06 09:47:31 +0200

    astronerbebidas

---
+++ data/latas.js
@@ -0,0 +1,31 @@
+export const latas1 = [{
+        slug: "lata1",
+        nombre: "NeonDrive Solar Rush",
+        img: "/img/Lata1.png",
+        desc: "La bebida ideal para sentirte en una verdadera fiesta..."
+    },
+    {
+        slug: "lata2",
+        nombre: "NeonDrive Zero Pulse",
+        img: "/img/Lata2.png",
+        desc: "Energía sin límites, sin azúcar."
+    },
+    {
+        slug: "lata3",
+        nombre: "NeonDrive Blue Voltage",
+        img: "/img/Lata3.png",
+        desc: "Siente la descarga eléctrica."
+    },
+    {
+        slug: "lata4",
+        nombre: "NeonDrive Toxic Surge",
+        img: "/img/Lata4.png",
+        desc: "Explosión de sabor extremo."
+    },
+    {
+        slug: "lata5",
+        nombre: "NeonDrive Night Charge",
+        img: "/img/Lata5.png",
+        desc: "Perfecta para noches largas."
+    }
+];
